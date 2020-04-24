ee6976c77ffaf1e6659e3e775067503e3dace93e
PaperDragon Red Work <r.badillo12@ciencias.unam.mx>
2020-04-23 20:01:33 -0500

    Practica6

--- app/src/main/java/com/example/practica1/ui/login/DisplayMessageActivity.java
+++ app/src/main/java/com/example/practica1/ui/login/DisplayMessageActivity.java
@@ -3,6 +3,9 @@ package com.example.practica1.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,41 +27,74 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        int pos = 0;
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.RESPONSE);
         StringTokenizer st = new StringTokenizer(message);
         lv = (ListView) findViewById(R.id.listView);
         while (st.hasMoreTokens()) {
-                ligas.add(st.nextToken());
+            String v = st.nextToken();
+            if(v.equals("true")){
+                v = st.nextToken();
             }
-        ligas.add("Nuevas");
-        ligas.add("https://www.youtube.com/watch?v=xMw1LHjAumo");
-        ligas.add("https://www.youtube.com/watch?v=RHEiY0rOQ2k");
-        ligas.add("https://www.youtube.com/watch?v=6pMW_0_vZIo");
-        ligas.add("https://www.youtube.com/watch?v=THwprYAmsNs");
-        ligas.add("https://www.youtube.com/watch?v=kkAh0ArGOt4");
-        ligas.add("https://www.youtube.com/watch?v=40kDkd2A2tc");
-        ligas.add("https://www.youtube.com/watch?v=3gDtTxdjRIM");
-        ligas.add("https://www.youtube.com/watch?v=G3rF4VwXImk");
-        ligas.add("Repetidas");
-        ligas.add("https://www.youtube.com/watch?v=xMw1LHjAumo");
-        ligas.add("https://www.youtube.com/watch?v=RHEiY0rOQ2k");
-        ligas.add("https://www.youtube.com/watch?v=6pMW_0_vZIo");
-        ligas.add("https://www.youtube.com/watch?v=THwprYAmsNs");
-        ligas.add("https://www.youtube.com/watch?v=kkAh0ArGOt4");
-        ligas.add("https://www.youtube.com/watch?v=40kDkd2A2tc");
-        ligas.add("https://www.youtube.com/watch?v=3gDtTxdjRIM");
-        ligas.add("https://www.youtube.com/watch?v=G3rF4VwXImk");
-        ligas.add("https://www.youtube.com/watch?v=xMw1LHjAumo");
-        ligas.add("https://www.youtube.com/watch?v=RHEiY0rOQ2k");
-        ligas.add("https://www.youtube.com/watch?v=6pMW_0_vZIo");
-        ligas.add("https://www.youtube.com/watch?v=THwprYAmsNs");
-        ligas.add("https://www.youtube.com/watch?v=kkAh0ArGOt4");
-        ligas.add("https://www.youtube.com/watch?v=40kDkd2A2tc");
-        ligas.add("https://www.youtube.com/watch?v=3gDtTxdjRIM");
-        ligas.add("https://www.youtube.com/watch?v=G3rF4VwXImk");
-
-        ligas.remove(0);
+            System.out.println(v);
+            if (v.indexOf("https://www.youtube.com") != -1) {
+                ligas.add(v);
+            }else{
+                if(pos == 0) {
+                    ligas.add("https://www.youtube.com/watch?v=xMw1LHjAumo");
+                    ligas.add("https://www.youtube.com/watch?v=RHEiY0rOQ2k");
+                    ligas.add("https://www.youtube.com/watch?v=6pMW_0_vZIo");
+                    ligas.add("https://www.youtube.com/watch?v=THwprYAmsNs");
+                    ligas.add("https://www.youtube.com/watch?v=kkAh0ArGOt4");
+                    ligas.add("https://www.youtube.com/watch?v=40kDkd2A2tc");
+                    ligas.add("https://www.youtube.com/watch?v=3gDtTxdjRIM");
+                    ligas.add("https://www.youtube.com/watch?v=G3rF4VwXImk");
+                    pos += 1;
+                }
+            }
+            if(v.indexOf("google.streetview")!=-1) {
+                ligas.add(v);
+            }else{
+                if(pos == 1) {
+                    ligas.add("google.streetview:cbll=19.223590,-99.030364");
+                    ligas.add("google.streetview:cbll=19.302501,-99.191553");
+                    pos += 1;
+                }
+            }
+            if(v.indexOf("?q=") == -1 && v.indexOf("geo:")!=-1) {
+                ligas.add(v);
+            }else{
+                if(pos == 2) {
+                    ligas.add("geo:19.311326,-99.196483");
+                    ligas.add("geo:19.324295,-99.179478");
+                    pos+=1;
+                }
+            }
+            if((v.indexOf("geo:0,0?q=")) != -1) {
+                ligas.add(v);
+            }else{
+                if(pos==3) {
+                    ligas.add("geo:0,0?q=cines");
+                    ligas.add("geo:0,0?q=escuelas");
+                    pos += 1;
+                }
+            }
+            if((v.indexOf("https://")) != -1 && pos == 4) {
+                ligas.add(v);
+            }else{
+                if(pos==4) {
+                    ligas.add("https://es.wikipedia.org/");
+                    ligas.add("https://twitter.com/");
+                    pos+=1;
+                }
+            }
+            if((v.indexOf("google.navigation")!=-1)){
+                ligas.add(v);
+                ligas.add("google.navigation:q=Universidad+Pedagogica+Nacional,+Mexico");
+                ligas.add("google.navigation:q=Prepa+8,+Mexico");
+            }
+        }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.activity_list,
@@ -69,7 +105,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
+                Uri liga = Uri.parse(ligas.get(position));
+                Intent intento = new Intent(Intent.ACTION_VIEW, liga);
                 Toast.makeText(DisplayMessageActivity.this,  ligas.get(position), Toast.LENGTH_LONG).show();
+                PackageManager packageManager = getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(intento, 0);
+                boolean isIntentSafe = activities.size() > 0;
+                if (isIntentSafe) {
+                    startActivity(intento);
+                }
+
             }
         });
     }
